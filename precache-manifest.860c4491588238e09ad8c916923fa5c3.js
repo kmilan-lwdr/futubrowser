b804ff85f902f254570dfa16c46a122985dbc843
Kim Milán <milan.knp@gmail.com>
2020-06-12 10:19:54 +0300

    Updates

---
+++ precache-manifest.860c4491588238e09ad8c916923fa5c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5c11ffcb3cb74cd2db4eee782ab3fd",
+    "url": "/futubrowser/index.html"
+  },
+  {
+    "revision": "7c6a4d22f8381049af48",
+    "url": "/futubrowser/static/css/main.877aea6c.chunk.css"
+  },
+  {
+    "revision": "9b83ef1d6d980b81d8ae",
+    "url": "/futubrowser/static/js/2.7ffe1d09.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/futubrowser/static/js/2.7ffe1d09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6a4d22f8381049af48",
+    "url": "/futubrowser/static/js/main.01e44446.chunk.js"
+  },
+  {
+    "revision": "e063d865abb92936e527",
+    "url": "/futubrowser/static/js/runtime-main.ec2fd7db.js"
+  },
+  {
+    "revision": "da010917cb085a33fe78c79b5c94d970",
+    "url": "/futubrowser/static/media/bg.da010917.svg"
+  },
+  {
+    "revision": "d768250149eb4edfb9d3104bb4e88437",
+    "url": "/futubrowser/static/media/frown.d7682501.svg"
+  },
+  {
+    "revision": "fda1130cfac03cc431faa52a964fdfba",
+    "url": "/futubrowser/static/media/loader.fda1130c.svg"
+  }
+]);
