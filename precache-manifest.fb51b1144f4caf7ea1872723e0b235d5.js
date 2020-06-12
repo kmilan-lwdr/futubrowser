f6690adb9c0a15db2169d844a06d763cd55c28c6
Kim Milán <milan.knp@gmail.com>
2020-06-12 13:15:41 +0300

    Updates

---
+++ precache-manifest.fb51b1144f4caf7ea1872723e0b235d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fefea6cf950fd3fbfbcae9cb639675",
+    "url": "/futubrowser/index.html"
+  },
+  {
+    "revision": "a8f29c64983b9cf872f8",
+    "url": "/futubrowser/static/css/main.9adf5496.chunk.css"
+  },
+  {
+    "revision": "91d3623d9caab7d899d3",
+    "url": "/futubrowser/static/js/2.eeea7c6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/futubrowser/static/js/2.eeea7c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8f29c64983b9cf872f8",
+    "url": "/futubrowser/static/js/main.18af9cd6.chunk.js"
+  },
+  {
+    "revision": "e063d865abb92936e527",
+    "url": "/futubrowser/static/js/runtime-main.ec2fd7db.js"
+  },
+  {
+    "revision": "da010917cb085a33fe78c79b5c94d970",
+    "url": "/futubrowser/static/media/bg.da010917.svg"
+  },
+  {
+    "revision": "d768250149eb4edfb9d3104bb4e88437",
+    "url": "/futubrowser/static/media/frown.d7682501.svg"
+  },
+  {
+    "revision": "fda1130cfac03cc431faa52a964fdfba",
+    "url": "/futubrowser/static/media/loader.fda1130c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.92bf605786d1767760ae501d7e85fd4b.js"
+  "/futubrowser/precache-manifest.fb51b1144f4caf7ea1872723e0b235d5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/futubrowser/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
